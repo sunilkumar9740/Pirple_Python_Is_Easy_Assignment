ea7abb4fd6a023d3a9cf50ab8b12c548a4f0e87e
sunilkumar9740 <59022363+sunilkumar9740@users.noreply.github.com>
2021-01-17 13:54:28 +0100

    Add files via upload

---
+++ varibles.py
@@ -0,0 +1,67 @@
+
+
+
+print("My favorite songs") #just im printing the heading for Favorite Song
+
+
+Artist='Harikrishna'
+Year_Released=2012
+Genre='Drama'
+DurationInSeconds=410
+
+print("My first Fav songs is")
+print(Artist)
+print(Year_Released)
+print(Genre)
+print(DurationInSeconds)
+
+print("--- end of my first favorite songs list---")
+
+Artist='Harikrishna'
+Acter='Ganesh'
+Actress='Bhavana'
+Year_Released=2010
+Genre='Drama'
+DurationInMinutes=4.10
+print("My Second Fav songs is")
+
+print(Artist)
+print(Acter)
+print(Actress)
+print(Year_Released)
+print(Genre)
+print(DurationInMinutes)
+
+print("--- end of my second favorite songs list---")
+
+
+x='--- end of my third favorite songs list--' #here im trying with global variable and function def
+
+def MyFavSong():
+	Artist='Arjun'
+	Year_Released='2011'
+	Genre='Romantic'
+	DurationInSeconds=310
+
+	print(Artist)
+	print(Year_Released)
+	print(Genre)
+	print(DurationInSeconds)
+
+MyFavSong()
+
+print(x)
+
+artist,Year_Released,genre='sambram',2013,'comedy'#here im trying to create in single like
+print(artist)
+print(Year_Released)
+print(genre)
+
+
+print(x)
+
+
+
+
+
+
